f944544a7c36a33b371c5f833e629b3164a7c2b8
Tom Futureworkz <tom@futureworkz.com>
2016-03-15 00:23:46 +0700

    Update seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 User.destroy_all
-Profile.destroy_all
 Post.destroy_all
-Comment.destroy_all
 
-tom = User.create(name: 'Tom', email: 'tom@futureworkz.com')
-tom_profile = tom.
+tom         = User.create(name: 'Tom', email: 'tom@futureworkz.com')
+tom_profile = tom.profile.create(
+                            display_name: 'Tom Ta',
+                            designation: 'Developer',
+                            dob: 23.years.ago,
+                            address: '1/4D Ba Trieu, Ap Dinh, Tan Xuan, Hoc Mon',
+                            city: 'Ho Chi Minh',
+                            gender: :male,
+                            mobile_number: '01675166477')
+
+ruby_post   = Post.create(title: 'I love Ruby', content: 'Ruby is awesome...', user: tom)
+rails_post  = Post.create(title: 'Rails is elegant', content: 'One of the most elegant web framework is Ruby on Rails...', user: tom)
