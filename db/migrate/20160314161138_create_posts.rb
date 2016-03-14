48a77b76a3e8d72c4e40137ea429c5a98d14add7
Tom Futureworkz <tom@futureworkz.com>
2016-03-14 23:16:39 +0700

    Add posts table

---
+++ db/migrate/20160314161138_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.references :user, index: true, foreign_key: true
+    end
+  end
+end
