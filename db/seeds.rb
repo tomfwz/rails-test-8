8b6c5e1c64d5f9cff875bac63af2f80fa78acf54
Tom Futureworkz <tom@futureworkz.com>
2016-03-15 22:18:14 +0700

    Update seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,20 @@
 User.destroy_all
 Post.destroy_all
 
-tom         = User.create(name: 'Tom', email: 'tom@futureworkz.com')
-tom_profile = tom.profile.create(
-                            display_name: 'Tom Ta',
-                            designation: 'Developer',
-                            dob: 23.years.ago,
-                            address: '1/4D Ba Trieu, Ap Dinh, Tan Xuan, Hoc Mon',
-                            city: 'Ho Chi Minh',
-                            gender: :male,
-                            mobile_number: '01675166477')
+tom         = User.create!(name: 'Tom',   password: '123123123', email: 'tom@futureworkz.com')
+wayne       = User.create!(name: 'Wayne', password: '123123123', email: 'wayne@futureworkz.com')
+james       = User.create!(name: 'James', password: '123123123', email: 'james@futureworkz.com')
+smith       = User.create!(name: 'smith', password: '123123123', email: 'smith@futureworkz.com')
 
-ruby_post   = Post.create(title: 'I love Ruby', content: 'Ruby is awesome...', user: tom)
-rails_post  = Post.create(title: 'Rails is elegant', content: 'One of the most elegant web framework is Ruby on Rails...', user: tom)
+tom_profile = Profile.create!(
+                        display_name: 'Tom Ta',
+                        designation: 'Developer',
+                        dob: 23.years.ago,
+                        address: '1/4D Ba Trieu, Ap Dinh, Tan Xuan, Hoc Mon',
+                        city: 'Ho Chi Minh',
+                        gender: :male,
+                        mobile_number: '01675166477',
+                        user: tom)
+
+ruby_post   = Post.create!(title: 'I love Ruby', content: 'Ruby is awesome...', user: tom)
+rails_post  = Post.create!(title: 'Rails is elegant', content: 'One of the most elegant web framework is Ruby on Rails...', user: tom)
