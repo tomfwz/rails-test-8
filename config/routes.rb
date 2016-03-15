dc5697f0986f2a14bb22d9a98f4b67c4c4e764bf
Tom Futureworkz <tom@futureworkz.com>
2016-03-15 22:49:28 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+
+  resources :users, only: [:show] do
+    resources :profiles, only: [:show, :new, :create]
+    resources :comments, only: [:index]
+  end
+
+  resources :posts, only: [:show] do
+    resources :comments, only: [:show, :new, :create]
+  end
+
+  resources :profiles, only: [:show] do
+    resources :comments, only: [:show, :new, :create]
+  end
 end
