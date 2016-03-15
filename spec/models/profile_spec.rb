2add7ecd189d900f6234e8ab0fe1bd73ba565d88
Tom Futureworkz <tom@futureworkz.com>
2016-03-15 22:38:01 +0700

    Add test case for testing the polymorphic has many comments in Profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -10,4 +10,15 @@ describe Profile do
     it { is_expected.to have_many :comments }
     it { is_expected.to belong_to :user }
   end
+
+  context 'has many comments' do
+    let!(:tom)          { create(:user, name: 'Tom', email: 'tom@futureworkz.com') }
+    let!(:james)        { create(:user, name: 'James', email: 'james@futureworkz.com') }
+    let!(:tom_profile)  { create(:profile, display_name: 'Tom Ta', designation: 'Developer', user: tom) }
+    let!(:comments)     { create_list(:comment, 3, user: james, message: 'Awesome', commentable: tom_profile) }
+
+    it 'returns the comments of a profile' do
+      expect(tom_profile.comments).to eq comments
+    end
+  end
 end
