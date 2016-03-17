ce303b90e1df92b798e6bd204dcc2f81e93e5a9c
Tom Futureworkz <tom@futureworkz.com>
2016-03-17 18:55:26 +0700

    Add CommentsController#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,28 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
+    @comment = current_user.comments.build(comment_params)
+    
+    respond_to do |format|
+      if @comment.save
+        format.html {
+          flash[:success] = 'Posting completed.'
+          redirect_to :back
+        }
+        format.js
+      else
+        format.html {
+          flash[:danger] = 'Error: An error has occurred in posting comment process.'
+          redirect_to :back
+        }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message, :commentable_id, :commentable_type)
   end
 end
