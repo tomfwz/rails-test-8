6966f6d3627bb5b8c971f67dc46224f0d9041985
Tom Futureworkz <tom@futureworkz.com>
2016-03-17 21:52:25 +0700

    Fix bug on required avatar in test cases

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name      'Tom'
     password  '123123123'
     email     { |n| "user-#{n}@example.com" }
+    avatar    File.open("#{Rails.root}/app/assets/images/users/avatars/medium/missing.png")
   end
 end
