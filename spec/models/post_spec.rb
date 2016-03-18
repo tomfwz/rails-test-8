8d830454c1a409099f5ce918c7b4384540ec8d5b
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 00:04:29 +0700

    Remove unneeded test case

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,15 +11,4 @@ describe Post do
     it { is_expected.to have_many :comments }
     it { is_expected.to belong_to :user }
   end
-
-  context 'has many comments' do
-    let!(:tom)      { create(:user, name: 'Tom', email: 'tom@futureworkz.com') }
-    let!(:james)    { create(:user, name: 'James', email: 'james@futureworkz.com') }
-    let!(:post)     { create(:post, title: 'I love Ruby', content: 'Ruby is awesome...', user: tom) }
-    let!(:comments) { create_list(:comment, 3, user: james, message: 'Awesome', commentable: post) }
-
-    it 'returns the comments of a post' do
-      expect(post.comments).to eq comments
-    end
-  end
 end
