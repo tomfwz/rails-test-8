f13fa7693873bf1338b0387df285d1d3982a8a3c
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 00:04:37 +0700

    Update users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
   factory :user do
-    name      'Tom'
-    password  '123123123'
-    email     { |n| "user-#{n}@example.com" }
-    avatar    File.open("#{Rails.root}/app/assets/images/users/avatars/medium/missing.png")
+    name          'Tom'
+    password      '123123123'
+    email         { |n| "user-#{n}@example.com" }
+    avatar        File.open("#{Rails.root}/app/assets/images/users/avatars/medium/missing.png")
+    display_name  'Tom Ta'
+    designation   'Developer'
+    dob           23.years.ago
+    address       '1/4D Ba Trieu, Ap Dinh, Tan Xuan, Hoc Mon'
+    city          'Ho Chi Minh'
+    gender        'male'
+    mobile_number '01675166477'
   end
 end
