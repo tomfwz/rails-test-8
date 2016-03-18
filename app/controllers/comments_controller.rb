23a728d213391a3301fe2ac2329edc7384caa139
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 00:37:30 +0700

    Refactor CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,56 +1,37 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :reply, :create_reply]
-  before_action :set_comment, only: [:reply, :create_reply]
+  before_action :authenticate_user!, only: [:new, :create]
+  
+  def new
+    @comment = Comment.new(parent_id: params[:parent_id])
+  end
+
   def create
-    @comment = current_user.comments.build(comment_params)
-    
+    if params[:comment][:parent_id].present?
+      parent = Comment.find(params[:comment][:parent_id])
+      @comment = parent.children.build(comment_params)
+      @comment.update_attributes(user_id: current_user.id)
+    else
+      @comment = Comment.new(comment_params)
+    end
+
     respond_to do |format|
       if @comment.save
         format.html {
-          flash[:success] = 'Posting completed.'
-          redirect_to :back
+          flash[:success] = 'Your comment was successfully added!'
+          redirect_to root_url
         }
         format.js
       else
         format.html {
           flash[:danger] = 'Error: An error has occurred in posting comment process.'
-          redirect_to :back
+          render :new
         }
         format.js
       end
     end
   end
 
-  def reply
-    @child_comment = @comment.children.new
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-
-  def create_reply
-    @child_comment = current_user.comments.build(message: message_param, parent_id: @comment.id)
-    if @child_comment.save
-      if @child_comment.commentable_type == 'Profile'
-        redirect_to @child_comment.user
-      else
-        redirect_to @child_comment.commentable
-      end
-    else
-      # render :reply
-    end
-  end
-
   private
-
-  def message_param
-    params[:comment][:message]
-  end
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:message, :commentable_id, :commentable_type)
