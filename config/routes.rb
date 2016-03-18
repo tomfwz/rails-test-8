b1cbd1fb1592f94ef4653587f6c951c85042d3d8
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 00:19:39 +0700

    Remove all profiles routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  resources :users, only: [:show] do
-    resources :profiles, only: [:show, :new, :create]
-  end
-
-  resources :posts,    only: [:show]
-  resources :profiles, only: [:show]
+  resources :users, only: [:show]
+  resources :posts, only: [:show]
 
   resources :comments, only: [:create] do
     member do
