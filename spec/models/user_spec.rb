dad24095a9f085e54129ca590b1be0c85fd1b952
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 00:04:45 +0700

    Update User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,10 @@ describe User do
   context 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :email }
+    it { is_expected.to enumerize(:gender).in(:male, :female) }
   end
 
   context 'associations' do
     it { is_expected.to have_many :comments }
-    it { is_expected.to have_one :profile }
   end
 end
