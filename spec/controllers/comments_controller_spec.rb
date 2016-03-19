341dc6c3a4b3b9034eccb119481a58d855e8d25b
Tom Futureworkz <tom@futureworkz.com>
2016-03-19 12:06:52 +0700

    Add CommentsController#create test case

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -26,4 +26,58 @@ describe CommentsController do
       expect(response).to render_template :new
     end
   end
+
+  describe '#create' do
+    def do_request
+      post :create, comment: comment
+    end
+
+
+    context 'creates a comment to a user profile' do
+      let!(:user)     { create(:user) }
+      let!(:comment)  { attributes_for(:comment, commentable_id: user.id, commentable_type: 'User', user_id: user.id) }
+
+      before { sign_in user }
+
+      it 'saves a comment' do
+        expect { do_request }.to change(Comment, :count).by(1)
+      end
+
+      it 'increases number of comments of a user profile' do
+        expect { do_request }.to change(user.comments, :count).by(1)
+      end
+    end
+
+    context 'creates a comment to a post' do
+      let!(:user)     { create(:user) }
+      let!(:post)     { create(:post, user_id: user.id) }
+      let!(:comment)  { attributes_for(:comment, commentable_id: post.id, commentable_type: 'Post', user_id: user.id) }
+
+      before { sign_in user }
+
+      it 'saves a comment' do
+        binding.pry
+        expect { do_request }.to change(Comment, :count).by(1) 
+      end
+
+      it 'increases number of comments of a post' do
+        expect { do_request }.to change(post.comments.reload, :count).by(1) 
+      end
+    end
+
+    context 'creates a comment as a reply' do
+      let!(:user)         { create(:user) }
+      let!(:post)         { create(:post, user_id: user.id) }
+      let!(:root_comment) { create(:comment, commentable_id: post.id, commentable_type: 'Post', user_id: user.id, parent_id: nil) }
+      let!(:comment)      { attributes_for(:comment, parent_id: root_comment.id, user_id: user.id) }
+
+      it 'saves a comment' do
+        expect { do_request }.to change(Comment, :count).by(1) 
+      end
+
+      it 'adds a comment to root comment' do
+        expect { do_request }.to change(root_comment.children, :count).by(1) 
+      end
+    end
+  end
 end
